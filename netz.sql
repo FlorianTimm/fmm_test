7969640b9213353a6cc9942563a39238bc22f75a
FlorianTimm <FlorianTimm@github.com>
2025-03-27 22:56:16 +0100

    Netz aufräumen

--- netz.sql
+++ netz.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS netz_pgr;
-DROP TABLE IF EXISTS netz_pgr_vertices_pgr;
+DROP TABLE IF EXISTS radnetz.netz_pgr;
+DROP TABLE IF EXISTS radnetz.netz_pgr_vertices_pgr;
 
-CREATE TABLE IF NOT EXISTS netz_pgr
+CREATE TABLE IF NOT EXISTS radnetz.netz_pgr
 (
     id SERIAL NOT NULL PRIMARY KEY,
     geom geometry(LINESTRING, 25832) NOT NULL,
@@ -15,25 +15,42 @@ CREATE TABLE IF NOT EXISTS netz_pgr
 	y2 decimal
 );
 
-CREATE INDEX ON netz_pgr USING gist (geom);
-CREATE INDEX ON netz_pgr USING btree (source);
-CREATE INDEX ON netz_pgr USING btree (target);
+CREATE INDEX ON radnetz.netz_pgr USING gist (geom);
+CREATE INDEX ON radnetz.netz_pgr USING btree (source);
+CREATE INDEX ON radnetz.netz_pgr USING btree (target);
 
-INSERT INTO netz_pgr (geom)
+INSERT INTO radnetz.netz_pgr (geom)
 SELECT (g).geom FROM (
-SELECT st_dump(st_node(st_collect(geom))) g FROM netz
-) t;
-
-INSERT INTO netz_pgr (geom)
-SELECT ST_REVERSE(geom) FROM netz_pgr;
+select st_dump(st_node(st_collect(
+--st_lineextend(
+(g).geom
+--,20,20)
+))) g from (
+SELECT st_dump(st_linemerge(st_collect(geom))) g FROM radnetz.radnetz_org
+));
 	
-SELECT pgr_createTopology('netz_pgr', 0.5, 'geom', clean := TRUE);
+SELECT pgr_createTopology('radnetz.netz_pgr', 10, 'geom', clean := TRUE);
 
-UPDATE netz_pgr SET 
+UPDATE radnetz.netz_pgr SET geom = ST_SETPOINT(geom, 0, (SELECT the_geom FROM radnetz.netz_pgr_vertices_pgr p WHERE source = p.id));
+
+
+UPDATE radnetz.netz_pgr SET geom = ST_SETPOINT(geom, -1, (SELECT the_geom FROM radnetz.netz_pgr_vertices_pgr p WHERE target = p.id));
+
+SELECT  pgr_analyzeGraph('radnetz.netz_pgr',10,'geom');
+
+SELECT pgr_createTopology('radnetz.netz_pgr_noded', 10, 'geom');
+SELECT  pgr_analyzeGraph('radnetz.netz_pgr_noded',10,'geom');
+
+
+UPDATE radnetz.netz_pgr SET 
 	cost = ST_LENGTH(geom),
 	x1 = ST_X(ST_StartPoint(geom)),
 	y1 = ST_Y(ST_StartPoint(geom)),
 	x2 = ST_X(ST_EndPoint(geom)),
 	y2 = ST_Y(ST_EndPoint(geom));
+
+	
+--INSERT INTO radnetz.netz_pgr (geom)
+--SELECT ST_REVERSE(geom) FROM radnetz.netz_pgr;
 
 
